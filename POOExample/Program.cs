4242038784a784d96b1ff856bfb5b599009f2ec1
Daniel Alonzo <daniel.alonzo@sedgwick.com>
2022-09-23 10:45:26 -0500

    Add project files.

---
+++ POOExample/Program.cs
@@ -0,0 +1,35 @@
+﻿using POOExample;
+
+var account = new BankAccount("Daniel", 1000);
+Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+
+account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+Console.WriteLine(account.Balance);
+account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+Console.WriteLine(account.Balance);
+
+// Test that the initial balances must be positive.
+BankAccount invalidAccount;
+try
+{
+    invalidAccount = new BankAccount("invalid", -55);
+}
+catch (ArgumentOutOfRangeException e)
+{
+    Console.WriteLine("Exception caught creating account with negative balance");
+    Console.WriteLine(e.ToString());
+    //return;
+}
+
+// Test for a negative balance.
+try
+{
+    account.MakeWithdrawal(750, DateTime.Now, "Attempt to overdraw");
+}
+catch (InvalidOperationException e)
+{
+    Console.WriteLine("Exception caught trying to overdraw");
+    Console.WriteLine(e.ToString());
+}
+
+Console.WriteLine(account.GetAccountHistory());
