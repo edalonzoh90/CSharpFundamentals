83af6678a65604fc75aeb69f54a20019e9595c6a
Daniel Alonzo <daniel.alonzo@sedgwick.com>
2022-09-27 17:50:02 -0500

    Added SOLID proyect

---
+++ SOLID.md
@@ -0,0 +1,13 @@
+# SOLID principles
+
+
+
+- ***Single Responsibility Principle (SRS)*** A class should only have a single responsibility, that is, only changes to one part of the software’s specification should be able to affect the specification of the class
+- ***Open/Closed Principle (OCP)***
+- ***Liskov Substitution Principle (LSP)***
+- ***Interface Segregation Principle (ISP)***
+- ***Dependency Inversion Principle (DIP)***
+
+[S.O.L.I.D. Principles of Object-Oriented Programming in C#](https://www.educative.io/blog/solid-principles-oop-c-sharp)
+[How to apply SOLID principles with practical examples in C#](https://enlabsoftware.com/development/how-to-apply-solid-principles-with-practical-examples-in-c-sharp.html)
+
---
+++ SOLIDExample/1.SingleResponibility.cs
@@ -0,0 +1,33 @@
+﻿using System.Data.SqlClient;
+using System.Net.Mail;
+
+namespace SOLIDExample
+{
+    public class RegisterService
+    {
+        // Does not follow SRP
+        public void RegisterUser(string username)
+        {
+            if (username == "admin")
+                throw new InvalidOperationException();
+
+            SqlConnection connection = new SqlConnection();
+            connection.Open();
+            SqlCommand command = new SqlCommand("INSERT INTO [...]");//Insert user into database. 
+
+            SmtpClient client = new SmtpClient("smtp.myhost.com");
+            client.Send(new MailMessage()); //Send a welcome email. 
+        }
+
+        public void RegisterUser(string username)
+        {
+            if (username == "admin")
+                throw new InvalidOperationException();
+            /*
+            _userRepository.Insert(...);
+
+            _emailService.Send(...);
+            */
+        }
+    }
+}
